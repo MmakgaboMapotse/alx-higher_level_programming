ebbcf23c7591dc8c4be5b7c659039565f944a3f9
mmakgabomapotse <mmakgabomapotse@gmail.com>
2024-04-24 13:31:25 -0700

    102-concat.js

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const sourceFile1 = process.argv[2];
+const sourceFile2 = process.argv[3];
+const destinationFile = process.argv[4];
+
+fs.readFile(sourceFile1, 'utf8', (err, data1) => {
+  if (err) {
+    console.error(`Error reading file ${sourceFile1}: ${err}`);
+    return;
+  }
+
+  fs.readFile(sourceFile2, 'utf8', (err, data2) => {
+    if (err) {
+      console.error(`Error reading file ${sourceFile2}: ${err}`);
+      return;
+    }
+
+    const concatenatedData = data1.trim() + ' ' + data2.trim();
+
+    fs.writeFile(destinationFile, concatenatedData, 'utf8', (err) => {
+      if (err) {
+        console.error(`Error writing to file ${destinationFile}: ${err}`);
+        return;
+      }
+
+      console.log(`Files ${sourceFile1} and ${sourceFile2} concatenated successfully to ${destinationFile}`);
+    });
+  });
+});
+
