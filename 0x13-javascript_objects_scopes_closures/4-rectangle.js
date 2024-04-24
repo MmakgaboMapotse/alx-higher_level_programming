a3756b70153c61b16859b83a78217e639f405da8
mmakgabomapotse <mmakgabomapotse@gmail.com>
2024-04-24 12:43:05 -0700

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return {};
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    if (!this.width || !this.height) return;
+
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate() {
+    if (!this.width || !this.height) return;
+
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double() {
+    if (!this.width || !this.height) return;
+
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
+
